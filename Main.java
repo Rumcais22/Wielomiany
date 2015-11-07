27960b7c4cbe586c7f58510632eedd45441a77e2
Your Name <you@example.com>
2015-11-07 17:06:15 +0100

    first commit

--- Main.java
+++ Main.java
@@ -6,11 +6,27 @@ public class Main {
 	// write your code here
         int a=2, b=-3, c=-8;
         double delta=b*b-4*a*c;
-        double x1=-b+Math.sqrt(delta)/2*a;
-        double x2=-b-Math.sqrt(delta)/2*a;
-        System.out.println(x1);
-        System.out.println(x2);
-        
+        if (delta==0){
+            System.out.println("To nie jest funkcja kwadratowa");
+        }else{
+
+            if (delta<0){
+                System.out.println("delta < 0");
+
+            }if(delta==0){
+                System.out.println("Rownanie ma 1 pierwiastek, x1: " + (-b/(2*a)) );
+            }if(delta>0){
+                System.out.println("x1: " + (-b-Math.sqrt(delta))/2*a );
+                System.out.println("x2: " + (-b+Math.sqrt(delta))/2*a );
+
+
+      
+
+
+
+
+            }
+        }
 
     }
 }
